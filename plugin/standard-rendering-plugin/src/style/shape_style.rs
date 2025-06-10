e3bfc6001ae2bbb3fcd4a3e8bf9e37a4094a8c74
Khachatur Yeritsyan <khachatur.yeritsyan@russianschool.com>
2025-06-10 19:48:33 +0400

    Refactoring.

--- plugin/standard-rendering-plugin/src/style/shape_style.rs
+++ plugin/standard-rendering-plugin/src/style/shape_style.rs
@@ -2,20 +2,29 @@ use crate::style::color::Color;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone)]
+pub struct StrokeStyle {
+    pub color: Color,
+    pub width: f64,
+    pub dash_array: Vec<u8>,
+    pub antialiasing: bool,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
 pub struct ShapeStyle {
     pub fill_color: Color,
-    pub stroke_color: Color,
-    pub stroke_width: f64,
-    pub stroke_dash_array: Vec<u8>,
+    pub stroke: StrokeStyle,
 }
 
 impl Default for ShapeStyle {
     fn default() -> Self {
         Self {
             fill_color: Color(0, 0, 0, 0),
-            stroke_color: Color(0, 0, 0, 255),
-            stroke_width: 1.0,
-            stroke_dash_array: vec![],
+            stroke: StrokeStyle {
+                color: Color(0, 0, 0, 255),
+                width: 1.0,
+                dash_array: vec![],
+                antialiasing: true
+            }
         }
     }
 }
--- plugin/standard-tool-plugin/src/tool/select_tool/render.rs
+++ plugin/standard-tool-plugin/src/tool/select_tool/render.rs
@@ -3,7 +3,7 @@ use entity_model_feature::entity_id::EntityId;
 use standard_rendering_plugin::renderable::Renderable;
 use standard_rendering_plugin::renderer::renderer::Renderer;
 use standard_rendering_plugin::style::color::Color;
-use standard_rendering_plugin::style::shape_style::ShapeStyle;
+use standard_rendering_plugin::style::shape_style::{ShapeStyle, StrokeStyle};
 
 impl<Id: EntityId> Renderable for SelectTool<Id> {
     fn render(&self, renderer: &mut dyn Renderer) {
@@ -11,11 +11,22 @@ impl<Id: EntityId> Renderable for SelectTool<Id> {
             return;
         };
 
+        let stroke_color: Color =
+            if selection.width() < 0.0 {
+                Color(0, 128, 0, 255)
+            } else {
+                Color(21, 69, 255, 255)
+            };
+
         renderer.rectangle(
             &selection,
             &ShapeStyle {
-                stroke_color: Color(0, 0, 255, 120),
-                stroke_dash_array: vec![2, 2],
+                stroke: StrokeStyle {
+                    color: stroke_color,
+                    width: 1.0,
+                    dash_array: vec![2, 2],
+                    antialiasing: false,
+                },
                 ..Default::default()
             },
         )
